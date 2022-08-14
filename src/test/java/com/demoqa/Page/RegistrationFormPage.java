9ab74ebbd340473782b88f1ff32993a1cc0d7832
Denis <chekulaev555@gmail.com>
2022-08-14 21:40:24 +0300

    work_PageObjects

---
+++ src/test/java/com/demoqa/Page/RegistrationFormPage.java
@@ -0,0 +1,2 @@
+package com.demoqa.Page;public class RegistrationFormPage {
+}
---
+++ src/test/java/com/demoqa/Page/conponents/CalendarComponent.java
@@ -0,0 +1,57 @@
+package com.demoqa.Page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationFormPage {
+
+    private SelenideElement
+            firstnameInput = $("#firstName"),
+            lastnameInput = $("#lastName"),
+            emailInput = $("#userEmail");
+
+
+    public RegistrationFormPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        return this;
+    }
+
+    public RegistrationFormPage setFirstName(String value) {
+        firstnameInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationFormPage setLastName(String value) {
+        lastnameInput.setValue(value);
+        return this;
+
+    }
+
+    public RegistrationFormPage setEmail(String value) {
+        emailInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationFormPage setGender(String value) {
+        $("#genterWrapper").$(byText(value)).click();
+        return this;
+    }
+    public RegistrationFormPage setBirthDate(String day,String month,String year){
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
+        return this;
+    }
+    public RegistrationFormPage setNumber(String valeu){
+        $("#userNumber").setValue(valeu);
+        return this;
+    }
+}
